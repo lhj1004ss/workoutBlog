252f02528d7b239e91bdcd2790f3e9d71c664115
hyojelee <hyojelee1004ss@gmail.com>
2023-07-09 20:52:31 -0700

    fixed middleware

--- front-end/store/configureStore.js
+++ front-end/store/configureStore.js
@@ -1,9 +1,15 @@
 import { createWrapper } from 'next-redux-wrapper';
-import { legacy_createStore as createStore } from 'redux'
-
-
+import { legacy_createStore as createStore, compose, applyMiddleware, } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from '../reducers'
+import { createLogger } from 'redux-logger'
+
+
 const configureStore = () => {
+  const enhancer = process.env.NODE_NEV === 'production' ? compose(applyMiddleware([]), createLogger()) :
+    composeWithDevTools(applyMiddleware([]), createLogger())
+  console.log('hello', enhancer)
+
   const store = createStore(reducer);
   return store
 }
