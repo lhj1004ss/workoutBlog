e282feb83301929f3f2e94be890e65c1f44d2196
hyojelee <hyojelee1004ss@gmail.com>
2023-11-13 17:13:15 -0800

    added signup saga

--- front-end/sagas/user.js
+++ front-end/sagas/user.js
@@ -44,16 +44,40 @@ function* logout() {
   }
 }
 
+function signupAPI() {
+  return axios.post('/api/signup')
+}
+
+function* signup() {
+  try {
+    // const result = yield call(logoutAPI);
+    yield delay(2000)
+
+    yield put({
+      type: actionTypes.SIGN_UP_SUCCESS,
+    })
+  } catch (err) {
+    yield put({
+      type: actionTypes.SIGN_UP_FAILURE,
+      error: err.response.data,
+    })
+  }
+}
+
 function* watchLogin() {
   yield takeLatest(actionTypes.LOG_IN_REQUEST, login)
 }
 function* watchLogout() {
   yield takeLatest(actionTypes.LOG_OUT_REQUEST, logout)
 }
+function* watchSignup() {
+  yield takeLatest(actionTypes.SIGN_UP_REQUEST, signup)
+}
 
 export default function* userSaga() {
   yield all([
     fork(watchLogin),
     fork(watchLogout),
+    fork(watchSignup),
   ])
 }
